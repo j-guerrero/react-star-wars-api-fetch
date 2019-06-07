cf044728735a070270f3eb31471a28d815a9bc44
Josh Guerrero <jrg99654@gmail.com>
2019-06-07 03:21:07 -0800

    Updates

---
+++ precache-manifest.90a68ccfbb5081a1c8f23c93251f3acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c818e379984749d763a5db6a07179f83",
+    "url": "/j-guerrero/react-star-wars-api-fetch.git/index.html"
+  },
+  {
+    "revision": "349a5fb9a4bf930577e1",
+    "url": "/j-guerrero/react-star-wars-api-fetch.git/static/css/main.7d324a61.chunk.css"
+  },
+  {
+    "revision": "36c6d2ff2f153374c56b",
+    "url": "/j-guerrero/react-star-wars-api-fetch.git/static/js/2.4834304d.chunk.js"
+  },
+  {
+    "revision": "349a5fb9a4bf930577e1",
+    "url": "/j-guerrero/react-star-wars-api-fetch.git/static/js/main.a794def3.chunk.js"
+  },
+  {
+    "revision": "50c27044bdece77e7a0f",
+    "url": "/j-guerrero/react-star-wars-api-fetch.git/static/js/runtime~main.900792f4.js"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/j-guerrero/react-star-wars-api-fetch.git/static/media/STARWARS.35f31c7a.woff"
+  }
+]);
